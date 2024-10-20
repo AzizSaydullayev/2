8bbd3b4874973f42c0904c12fba2d345b6b17b55
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-10-20 23:04:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function btnClick (id){
+switch (id) {
+    case 1:
+    alert("1-button")    
+        break;
+        case 2:
+            alert("2-button")    
+                break;
+                case 3:
+                    alert("3-button")    
+                        break; 
+                        case 4:
+                            alert("4-button")    
+                                break;   
+                                case 5:
+                                    alert("5-button")    
+                                        break; 
+                                        case 6:
+                                            alert("6-button")    
+                                                break; 
+                                                case 7:
+                                                    alert("7-button")    
+                                                        break; 
+                                                        case 8:
+                                                            alert("8-button")    
+                                                                break;   
+                                                                case 9:
+                                                                    alert("9-button")    
+                                                                        break;  
+                                                                        case 10:
+                                                                            alert("10-button")    
+                                                                                break;                                                                                                                                                                                                                                                                                                                                               
+    default:
+    alert("Boshqa button tanlang")
+        break;
+}
+}
